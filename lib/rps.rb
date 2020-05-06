070760c490c909b889860641b7078042474503d5
austinb847 <austinb847@icloud.com>
2020-05-06 10:50:46 -0700

    meaningful fail for wins? method

---
+++ lib/rps.rb
@@ -0,0 +1,8 @@
+class RPS
+  def wins?(obj, arg)
+    # if (obj == "rock") & (arg == "scissors")
+    #   true
+    # end
+  end
+  
+end
---
+++ spec/rps_spec.rb
@@ -0,0 +1,10 @@
+require('rps')
+require('rspec')
+
+
+describe("RPS#wins?") do
+  it("returns true if rock is the object and scissors is the argument") do
+  game = RPS.new()
+  expect(game.wins?("rock", "scissors")).to(eq(true))
+  end
+end
