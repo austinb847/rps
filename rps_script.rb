81e0b04c1a80defcb04a9b5b82af8adc5f4e8004
austinb847 <austinb847@icloud.com>
2020-05-06 17:43:10 -0700

    print what the computer generates to the console to make game more readable

--- rps_script.rb
+++ rps_script.rb
@@ -4,11 +4,13 @@ game = RPS.new()
 
 while game.game_running
   #puts game.round_number
+  puts "========================================================================"
   if game.round_number > 3
     game.game_running = false
   end
   player_choice = game.get_player_choice()
-  npc_choice = game.generate_rps() 
+  npc_choice = game.generate_rps()
+  puts "computer chooses: #{npc_choice}"
   check_win = game.wins?(npc_choice, player_choice)
   
   if check_win == false
@@ -28,9 +30,9 @@ while game.game_running
 end
 
 if game.player_score > game.npc_score
-  puts "You won the game!"
+  puts "You won the game! The final score was #{game.player_score} to #{game.npc_score}"
 elsif game.player_score < game.npc_score
-  puts "You lost the game :("
+  puts "You lost the game :( The final score was #{game.npc_score} to #{game.player_score}"
 else
-  puts "Tie game!"
+  puts "Tie game! You had a score of #{game.player_score} and I had a score of #{game.npc_score}"
 end
